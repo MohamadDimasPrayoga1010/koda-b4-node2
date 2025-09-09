e69e6b8ece5a36e1b0f24ebbf4d0208b8cc85c05
M.Dimas Prayoga <mdimasprayoga692@gmail.com>
2025-09-09 23:34:16 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const readline = require("readline");
+const convert = require("./lib/external");
+const noConvert = require("./lib/tanpaExternal");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Masukkan Tanggal (DD-MM-YY) ", (tanggal) => {
+  //   convert.convertDateExternal(tanggal)
+  noConvert.tanpaEksternal(tanggal);
+
+  rl.close();
+});
+
