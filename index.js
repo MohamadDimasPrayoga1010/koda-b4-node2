95fedf5cadfe0e213dcb16c13c8810a9b9282245
M.Dimas Prayoga <mdimasprayoga692@gmail.com>
2025-09-10 11:09:38 +0700

    update

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const readline = require("readline");
-const convert = require("./lib/external");
-const noConvert = require("./lib/tanpaExternal");
+const readline = require("readline")
+const convert = require("./lib/external")
+const noConvert = require("./lib/tanpaExternal")
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-});
+})
 
 rl.question("Masukkan Tanggal (DD-MM-YY) ", (tanggal) => {
   //   convert.convertDateExternal(tanggal)
-  noConvert.tanpaEksternal(tanggal);
+  noConvert.tanpaEksternal(tanggal)
 
-  rl.close();
-});
+  rl.close()
+})
 
